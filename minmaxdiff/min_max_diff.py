4c69e79c7a3c6e0528365e5aefb8cc9f91abb615
nemeth-bence <nemeth.bence@alwt.hu>
2017-04-20 11:43:41 +0200

    minmaxdiff exercise - function created (need to fix)

--- minmaxdiff/min_max_diff.py
+++ minmaxdiff/min_max_diff.py
@@ -1,3 +1,15 @@
 # Create a function called `min_max_diff` that takes a list of numbers as parameter
 # and returns the difference between maximum and minimum values in the list
 # Create basic unit tests for it with at least 3 different test cases
+
+def min_max_diff(numbers):
+    difference = []
+    try:
+        for number in range(len(numbers)):
+            if max(numbers) - min(numbers) == number:
+                difference.append(number)
+        return sum(difference)
+    except ZeroDivisionError:
+        return None
+
+min_max_diff([4,7,3,2,9])
