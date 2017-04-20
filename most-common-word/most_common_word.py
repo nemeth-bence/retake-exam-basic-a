5269231b84cdda16255aea7d56f93a75db0b542b
nemeth-bence <nemeth.bence@alwt.hu>
2017-04-20 13:23:16 +0200

    Most common word exercise - First part is done, I have an idea how to follow.

--- most-common-word/most_common_word.py
+++ most-common-word/most_common_word.py
@@ -3,4 +3,17 @@
 # python most_common_word.py [source]
 # When the argument provided and the source is a file
 # count all words in the given file and print the most common
-# ("cat", "CAT", "cat," "cat." are different words ) 
+# ("cat", "CAT", "cat," "cat." are different words )
+
+import sys
+
+words = 0
+
+if len(sys.argv) == 1:
+    print('Use this command: python most_common_word.py [source]')
+else:
+    try:
+        file = open(sys.argv[1], 'r')
+        text_string = textfile.read().rstrip()
+        text_list = text_string.split("")
+        word_counter = {}
