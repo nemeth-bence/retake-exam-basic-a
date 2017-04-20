f43690a0d25f05d73abb0f793925d0dbf8b2bcc7
nemeth-bence <nemeth.bence@alwt.hu>
2017-04-20 13:54:26 +0200

    Minmaxdiff ex. and test refactored, Most common ex. almost done

--- most-common-word/most_common_word.py
+++ most-common-word/most_common_word.py
@@ -13,7 +13,19 @@ if len(sys.argv) == 1:
     print('Use this command: python most_common_word.py [source]')
 else:
     try:
-        file = open(sys.argv[1], 'r')
+        textfile = open(sys.argv[1], 'r')
         text_string = textfile.read().rstrip()
-        text_list = text_string.split("")
+        text_list = text_string.split(" ")
         word_counter = {}
+        for word in text_list:
+            if word in word_counter:
+                word_counter[word] += 1
+            else:
+                word_counter[word] = 1
+        textfile.close()
+
+        most_common_word = max(word_counter, key = word_counter.get)
+        print(most_common_word, word_counter[most_common_word])
+
+    except FileNotFoundError:
+        print('No source file.')
